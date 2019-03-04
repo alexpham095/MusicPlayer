96cee1321c7583e5293f4e4a6b7d4cf3167567bf
Alex Pham <alexpham095@gmail.com>
2019-03-03 22:12:18 -0800

    Initial commit

---
+++ src/MusicPlayerGUI.java
@@ -0,0 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+
+public class MusicPlayerGUI extends JFrame{
+
+    BasicPlayer player;
+
+    JFrame  main = new JFrame("Streamplayer by Alex Pham");
+    JTable table;
+    JScrollPane scrollPane;
+    JButton play;
+    JPanel buttonPanel;
+    int CurrentSelectedRow;
+
+    public MusicPlayerGUI(){
+        player = new BasicPlayer();
+
+    }
+
+}
