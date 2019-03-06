9a1edc54505b08f3b4ea6ed412efd71332032e6b
Alex Pham <alexpham095@gmail.com>
2019-03-05 22:16:13 -0800

    Got music to start playing and library to be available.

---
+++ src/Database.java
@@ -0,0 +1,141 @@
+import com.mpatric.mp3agic.*;
+
+import javax.swing.table.DefaultTableModel;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Vector;
+
+public class Database {
+    private static String dbURL = "jdbc:derby:SongsDB;create=true";
+    private static String tableName = "Songs";
+    // jdbc Connection
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    protected static void createConnection()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL);
+        }
+        catch (Exception except)
+        {
+            except.printStackTrace();
+        }
+    }
+
+    protected static void buildTable()
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE " + tableName + " (File CHAR(150), " + "Title CHAR(150), " + "Artist CHAR(100) " + ")");
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    }
+
+    protected static void insertSong()
+    {
+        try
+        {
+            File file = new File("C:\\Users\\aPham\\Google Drive\\CSULB\\CECES 543\\MusicPlayer\\javaMP\\KDA_POPSTARS_INSTRUMENTAL_320kbps.mp3");
+            String songPath = file.getAbsolutePath();
+
+
+            Mp3File mp3file = new Mp3File(songPath);
+            String title = " ";
+            String artist = " ";
+            if(mp3file.hasId3v1Tag()){
+                ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+                title = id3v1Tag.getTitle();
+                artist = id3v1Tag.getArtist();
+            }
+            if(mp3file.hasId3v2Tag()){
+                ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+                title = id3v2Tag.getTitle();
+                artist = id3v2Tag.getArtist();
+            }
+
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO " + tableName + " values (?,?,?)");
+            statement.setString(1,songPath);
+            statement.setString(2,title);
+            statement.setString(3,artist);
+            statement.executeUpdate();
+
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        } catch (UnsupportedTagException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static DefaultTableModel buildSongs()
+    {
+        Vector<Vector<Object>> output = new Vector<Vector<Object>>();
+        Vector<String> columnNames = new Vector<String>();
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("select * from " + tableName);
+            ResultSetMetaData rsmd = results.getMetaData();
+            int numberCols = rsmd.getColumnCount();
+            for (int i=1; i<=numberCols; i++)
+            {
+                //print Column Names
+                columnNames.add(rsmd.getColumnLabel(i));
+            }
+
+            System.out.println("\n-------------------------------------------------");
+
+            while(results.next())
+            {
+                Vector<Object> vector = new Vector<Object>();
+                for (int columnIndex = 1; columnIndex <= numberCols; columnIndex++) {
+                    vector.add(results.getObject(columnIndex));
+                }
+                output.add(vector);
+            }
+            results.close();
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+        return new DefaultTableModel(output, columnNames);
+    }
+
+    protected static void shutdown()
+    {
+        try
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+            if (conn != null)
+            {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                conn.close();
+            }
+        }
+        catch (SQLException sqlExcept)
+        {
+
+        }
+
+    }
+}
--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -2,8 +2,11 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,6 +22,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
 
+import com.mpatric.mp3agic.*;
 import javazoom.jlgui.basicplayer.BasicPlayer;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 
@@ -26,16 +30,137 @@ public class MusicPlayerGUI extends JFrame{
 
     BasicPlayer player;
 
-    JFrame  main = new JFrame("Streamplayer by Alex Pham");
+    String songPath;
+    JFrame  main = new JFrame("Music Player");
     JTable table;
     JScrollPane scrollPane;
-    JButton play;
-    JPanel buttonPanel;
+    JButton play, stop, pause, skip, previous;
+    JPanel buttonPanel, musicPanel;
+    ButtonListener bl;
     int CurrentSelectedRow;
+    Database data;
 
-    public MusicPlayerGUI(){
+    public MusicPlayerGUI(Database database) {
+        data = database;
         player = new BasicPlayer();
+        buttonPanel = new JPanel();
+        musicPanel = new JPanel();
+        main.setLayout(new BorderLayout());
+        bl=new ButtonListener();
 
+        //table creation and mouseListener.
+        table = new JTable(data.buildSongs());
+        MouseListener mouseListener = new MouseAdapter() {
+            //this will print the selected row index when a user clicks the table
+            public void mousePressed(MouseEvent e) {
+                CurrentSelectedRow = table.getSelectedRow();
+                songPath = (table.getValueAt(CurrentSelectedRow,0)).toString();
+            }
+        };
+        table.addMouseListener(mouseListener);
+        TableColumn column = table.getColumnModel().getColumn(0);
+        column.setPreferredWidth(250);
+        column = table.getColumnModel().getColumn(1);
+        column.setPreferredWidth(20);
+        scrollPane = new JScrollPane(table);
+
+        //play button
+        play = new JButton(">");
+        play.addActionListener(bl);
+
+        stop = new JButton("[]");
+        stop.addActionListener(bl);
+
+        pause = new JButton("=");
+        pause.addActionListener(bl);
+
+        skip = new JButton(">|");
+        skip.addActionListener(bl);
+
+        previous = new JButton("|<");
+        previous.addActionListener(bl);
+
+        buttonPanel.add(previous);
+        buttonPanel.add(pause);
+        buttonPanel.add(stop);
+        buttonPanel.add(play);
+        buttonPanel.add(skip);
+        main.setSize(700,200);
+        main.add(scrollPane, BorderLayout.CENTER);
+        main.add(buttonPanel, BorderLayout.SOUTH);
     }
 
+    public void go(){
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setVisible(true);
+    }
+
+    class ButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(">".equals(e.getActionCommand())) {
+                try {
+                    Mp3File mp3file = new Mp3File(songPath);
+                    player.open(new File(songPath));
+                    player.play();
+                    System.out.println(player.getStatus());
+                } catch (BasicPlayerException ex) {
+                    System.out.println("BasicPlayer exception");
+                    Logger.getLogger(MusicPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedTagException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                } catch (InvalidDataException e1) {
+                    e1.printStackTrace();
+                }
+            } else if("=".equals(e.getActionCommand())) {
+                try {
+                    if(player.getStatus() == 1){
+                        player.resume();
+                    } else if(player.getStatus() == 0){
+                        player.pause();
+                    }
+                    System.out.println(player.getStatus());
+
+                } catch (BasicPlayerException ex) {
+                    System.out.println("BasicPlayer exception");
+                    Logger.getLogger(MusicPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else if("[]".equals(e.getActionCommand())) {
+                try {
+                    player.stop();
+                    System.out.println(player.getStatus());
+                } catch (BasicPlayerException ex) {
+                    System.out.println("BasicPlayer exception");
+                    Logger.getLogger(MusicPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else if("|<".equals(e.getActionCommand())) {
+                try {
+                    songPath = (table.getValueAt(--CurrentSelectedRow,0)).toString();
+                    player.open(new File(songPath));
+                    player.play();
+                    System.out.println(player.getStatus());
+                } catch (BasicPlayerException ex) {
+                    System.out.println("BasicPlayer exception");
+                    Logger.getLogger(MusicPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            else if(">|".equals(e.getActionCommand())) {
+                try {
+                    songPath = (table.getValueAt(++CurrentSelectedRow,0)).toString();
+                    player.open(new File(songPath));
+                    player.play();
+                    System.out.println(songPath);
+                    System.out.println(player.getStatus());
+                } catch (BasicPlayerException ex) {
+                    System.out.println("BasicPlayer exception");
+                    Logger.getLogger(MusicPlayerGUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
 }
